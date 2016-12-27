2c8cf83ebd8f2d3e72228e4266d30c92199d5b9b
SuperApollo <wpb504@126.com>
2016-12-27 15:37:54 +0800

    socket搭建

--- app/src/main/java/com/apollo/apollopaste/MainActivity.java
+++ app/src/main/java/com/apollo/apollopaste/MainActivity.java
@@ -3,15 +3,23 @@ package com.apollo.apollopaste;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.ServerSocket;
@@ -20,7 +28,6 @@ import java.net.SocketException;
 import java.util.Enumeration;
 
 public class MainActivity extends Activity {
-
     private TextView mTvShow;
     private EditText mEtIp;
     private EditText mEtPort;
@@ -30,6 +37,11 @@ public class MainActivity extends Activity {
     private Context mContext;
     private ToastUtils mToastUtils;
     private boolean mThreadStart;
+    private Handler mHandler;
+    private final int SOCKET_CONNECTED = 10086;
+    private final int SOCKET_DISCONNECTED = 10087;
+    private final int RECEIVE_SOCKET = 10088;
+    private final int SOCET_ERR = 10089;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +50,28 @@ public class MainActivity extends Activity {
         mContext = BaseApplication.getContext();
         mToastUtils = ToastUtils.shareInstance();
         initView();
+        mHandler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                switch (msg.what) {
+                    case SOCKET_CONNECTED:
+                        mToastUtils.show(mContext, "连接成功！");
+                        break;
+                    case SOCKET_DISCONNECTED:
+                        mToastUtils.show(mContext, "连接失败！");
+                        break;
+                    case RECEIVE_SOCKET:
+                        String receiveStr = (String) msg.obj;
+                        mTvShow.setText("本机ip地址：" + getLocalIpAddress() + "\n" + receiveStr);
+                        break;
+                    case SOCET_ERR:
+                        String errMsg = (String) msg.obj;
+                        mToastUtils.show(mContext, errMsg);
+                        break;
+                }
+
+            }
+        };
     }
 
     private void initView() {
@@ -91,8 +125,9 @@ public class MainActivity extends Activity {
      * 服务端
      */
     private void startSocketServer() {
-        mToastUtils.show(mContext, "开启服务端");
+
         if (!mThreadStart) {
+            mToastUtils.show(mContext, "开启服务线程");
             mThreadStart = true;
             new Thread() {
                 /* (non-Javadoc)
@@ -101,48 +136,70 @@ public class MainActivity extends Activity {
                 @Override
                 public void run() {
                     // TODO Auto-generated method stub
-                    ServerSocket serverSocket = null;
                     try {
-                        //创建ServerSocket对象监听6688端口
-                        serverSocket = new ServerSocket(6688);
-                        //接收tcp连接返回socket对象
-                        Socket socket = serverSocket.accept();
-                        if (socket.isConnected()) {
-                            runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    mToastUtils.show(mContext, "连接成功！");
+                        //创建ServerSocket对象监听8888端口
+                        ServerSocket serverSocket = new ServerSocket(8888);
+                        while (true) {
+                            //接收tcp连接返回socket对象
+                            Socket client = serverSocket.accept();
+                            System.out.println("S: Receiving...");
+                            if (client.isConnected()) {
+                                mHandler.sendEmptyMessage(SOCKET_CONNECTED);
+                            } else {
+                                mHandler.sendEmptyMessage(SOCKET_DISCONNECTED);
+                            }
+                            try {
+                                //接收客户端消息
+                                InputStream inputStream = client.getInputStream();
+                                BufferedReader in = new BufferedReader(new InputStreamReader(inputStream));
+                                // 发送给客户端的消息
+                                PrintWriter out = new PrintWriter(new BufferedWriter(
+                                        new OutputStreamWriter(client.getOutputStream())), true);
+//                        byte[] byteBuffer = new byte[1024];
+//                        StringBuilder sb = new StringBuilder();
+//                        int temp;
+//                        //读取接收的数据
+//                        while ((temp = inputStream.read(byteBuffer)) != -1) {
+//                            sb.append(new String(byteBuffer, 0, temp));
+//                            Log.i(MainActivity.class.getSimpleName(), new String(byteBuffer, 0, temp));
+//                        }
+//                        final String receiveStr = sb.toString();
+//                        Log.i(MainActivity.class.getSimpleName(), receiveStr);
+                                String inString = in.readLine();
+                                Log.i(MainActivity.class.getSimpleName(), inString);
+                                Message msg = new Message();
+                                msg.what = RECEIVE_SOCKET;
+                                msg.obj = inString;
+                                mHandler.sendMessage(msg);
+                                //将接收到的消息发送给客户端
+                                if (!TextUtils.isEmpty(inString)) {
+                                    out.println("from server: " + inString);
+                                    out.flush();
                                 }
-                            });
-
-                        } else {
-                            runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    mToastUtils.show(mContext, "连接失败！");
-                                }
-                            });
+                            }catch (Exception e){
+                                e.printStackTrace();
+                            }finally {
+                                client.close();
+                                serverSocket.close();
+                            }
 
                         }
-                        //获得输入流
-                        InputStream inputStream = socket.getInputStream();
-                        byte[] byteBuffer = new byte[1024];
-                        int temp = 0;
-                        //读取接收的数据
-                        while ((temp = inputStream.read(byteBuffer)) != -1)
-                            System.out.println(new String(byteBuffer, 0, temp));
-                        socket.close();
-                        serverSocket.close();
+
 
                     } catch (IOException e) {
                         e.printStackTrace();
-                        mToastUtils.show(mContext, e.toString());
+                        Message msg = new Message();
+                        msg.what = SOCET_ERR;
+                        msg.obj = e.getMessage();
+                        mHandler.sendMessage(msg);
                         mThreadStart = false;
                     }
                 }
 
             }.start();
 
+        } else {
+            mToastUtils.show(mContext, "服务线程运行中！");
         }
     }
 
